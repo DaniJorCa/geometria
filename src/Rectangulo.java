0556432ec5582be7797de0171dcbeabfcb7f8f3c
Daniel <daniel.jorca@gmail.com>
2023-02-19 14:15:02 +0100

    versión comentada

--- src/Rectangulo.java
+++ src/Rectangulo.java
@@ -1,19 +1,36 @@
-
+/**
+ * @author Daniel Jordan Carreño
+ * @version 1.2
+ */
 public class Rectangulo extends FiguraGeometrica {
 	private double l1;
 	private double l2;
-	
+
+	/**
+	 *
+	 * @param tipoFigura String descripcion figura
+	 * @param lG double del lado del lado grande
+	 * @param lP double del lado pequeño
+	 */
 	public Rectangulo(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+	/**
+	 *
+	 * @return Devuelve un primitivo double indicandonos el area
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+
+	/**
+	 *
+	 * @return Devuelve un primitivo double indicandonos el perimetro
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
